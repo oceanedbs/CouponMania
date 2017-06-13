afa2dc6ec902f487bb7def18f8587bfc0fe766af
Unknown <oceane.tribudubois@gmail.com>
2017-06-13 17:42:28 +0200

    Bug correction

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -24,6 +24,7 @@ class AdminController extends Zend_Controller_Action
 
 	public function indexAction()
 	{
+            $coupon=$this->_adminModel->numeroCoupon();
 
             $coupon=$this->_adminModel->numeroCoupon();
             $aziende=$this->_adminModel->numeroAziende();
@@ -105,4 +106,98 @@ class AdminController extends Zend_Controller_Action
 				));
 		return $this->_form2;
 	}
+        
+      
+
+	
+        
+        public function modificaaziendeAction(){
+    
+    }
+    public function modificaAction(){
+    if (!$this->getRequest()->isPost()) {
+			$this->_helper->redirector('index');
+		}
+		$form=$this->_modifica;
+		if (!$form->isValid($_POST)) {
+			return $this->render('modificaaziende');
+		}
+		$values = $form->getValues();
+		$this->_adminModel->modificaAziende($values,$this->getParam('piva'));
+		$this->_helper->redirector('visualizzaaziende');
+    }
+private function getModificaAziendeForm()
+	{
+		$urlHelper = $this->_helper->getHelper('url');
+		$this->_modifica = new Application_Form_Admin_Aziende_Modificaaziende();
+		$this->_modifica->setAction($urlHelper->url(array(
+				'controller' => 'admin',
+				'action' => 'modifica'),
+				'default'
+				));
+                $idazienda=$this->_getParam('piva',null);
+                $azienda=$this->_adminModel->getInfoAzienda($idazienda)->current()->toArray();
+		return $this->_modifica->populate($azienda);
+             
+	}
+
+ public function cancellaAction(){
+     $this->_adminModel->cancellaAzienda($this->getParam('piva'));
+     $this->_helper->redirector('visualizzaaziende');
+     
+ }
+
+
+    
+    
+
+    //visualizzazione promozioni
+    public function visualizzaaziendeAction(){
+    $page=null;
+    $aziende=$this->_adminModel->getAziende($page);
+    
+    $this->view->assign(array(
+            		'azienda' => $aziende,
+                        )
+            );
+    
+		
+    }
+	public function statAction()
+	{
+	}
+	public function statisticaAction()
+	{
+            $mod=$this->_getParam('mod', null);
+            $couponutente=array();
+            $couponpromo=array();
+            
+            $utente = $this->_adminModel->getUtente();
+                        
+            $idutente = $this->_getParam('idutente', null);
+            $paged=$this->_getParam('page', null);
+            $idpromo = $this ->_getParam('idpromo', null);
+            
+            $promozioni=$this->_adminModel->getProds($paged);
+            
+            if($mod == 2){
+            
+                $couponutente = $this->_adminModel->getCouponUtente($idutente);
+            
+            }
+            else{
+            
+                $couponpromo = $this->_adminModel->getCouponPromo($idpromo);
+            
+            }
+            $this->view->assign(array(
+            		'mod' => $mod,
+            		'utente' => $utente,
+            		'idutente'=>$idutente,
+            		'couponutente' => $couponutente,
+            		'promozioni' => $promozioni,
+            		'idpromo'=> $idpromo,
+                        'couponpromo'=>$couponpromo,)
+            		);
+            }
 }
