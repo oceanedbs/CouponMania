08458dcb31999207e0040cf2d1f12182d89dd0ca
Unknown <oceane.tribudubois@gmail.com>
2017-06-15 19:43:46 +0200

    Admin modifica il suo profilo ma non lo cancelo

---
+++ application/forms/Admin/Profilo/Profilo.php
@@ -0,0 +1,21 @@
+<?php
+class Application_Form_Admin_Profilo extends Zend_Form
+{
+
+	public function init()
+	{
+            $this->setMethod('post');
+		$this->setName('stampare');
+		$this->setAction('');
+		$this->setAttrib('enctype', 'multipart/form-data');
+
+
+            $this->addElement('submit', 'cambiareprofilo', array(
+                                'label' => ' Cambiare le Credenziali ',
+		));
+		
+		
+        }
+        
+        
+}
